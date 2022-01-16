b9e03d4c1a0633688e4a0e2ff27b560765bae1a4
Kamilolek <90256197+Kamilolek@users.noreply.github.com>
2022-01-16 16:27:56 +0100

    Update show.php

--- show.php
+++ show.php
@@ -9,15 +9,16 @@
         #main{
         margin: 0 auto;
         color:#f7d2ad;
-        width: 60%;
+        width: auto;
         background-color: #2f4858;
         text-align: center;
-        height: 30vh;
+        height: 100%;     
+        padding-top:1vh;       
+        padding-bottom:3vh;
         border-radius: 1vh;
-        margin-top:30vh;
-        margin-bottom:30vh;
+        margin-top:3vh;       
+        margin-bottom:3vh;
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-        padding-top:10vh;
 }
         body{
         font-family: Arial;
@@ -26,8 +27,70 @@
         font-size: 3vh;
         text-align: center;
 }
-
-
+table{
+    margin-left: auto;
+    margin-right: auto;
+}
+.ggez{
+    border:#f7d2ad 2px solid;
+    border-collapse: collapse;
+    height:auto;
+    width:auto;
+    font-size: 2vh;
+    margin-left: 2vh;
+    margin-right: 2vh;
+}
+input[type=text],input[type=number]{
+         background-color: #2f4858;
+         border:none;
+         border-bottom: #f7d2ad 2px solid;
+         color:#f7d2ad;
+         text-align: center;
+         font-size: 3vh;
+         margin-bottom: 1vh;
+}
+        input:focus {
+         outline: none;
+}
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+         margin: 0;
+}
+        select:focus {
+         outline: none;
+}
+        select::-webkit-outer-spin-button,
+        select::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+         margin: 0;
+         background-color:none;
+}
+        input[type=submit] {
+        font-family: Arial;
+        background-color: #2f4858;
+        color:#f7d2ad;
+        font-size: 3vh;
+        border:#f7d2ad 2px solid;
+        margin-top:1vh;
+        transition: 1s;
+        margin-bottom:3vh;
+}
+        input[type=submit]:hover {
+        background-color:#f7d2ad ;
+        color:#2f4858;
+        cursor: pointer;
+}
+#sql{
+        background-color: #2f4858;
+        color:#f7d2ad;
+        border:none;
+        border:#f7d2ad 2px solid;
+        font-size: 3vh;
+        text-align: center;
+        margin-top:2vh;
+        margin-bottom: 3vh;
+}
     </style>
 </head>
 <body>
@@ -35,29 +98,37 @@
        <h1>Wyszukiwarka</h1>
 
        <form method="post" action="show.php">
-           <table>
+       <table>
                <tr>
-                   <td><label>Imię: </label></td><td><input type="text" name="name"></td>
+                <td><label>Imię:</label></td>
+                </tr>
+                <tr>
+                   <td><input type="text" name="name"></td>
                </tr>
                <tr>
-                <td><label>Nazwisko: </label></td><td><input type="text" name="lname"></td>
+                <td><label>Nazwisko:</label></td>
+            </tr>
+            <tr><td><input type="text" name="lname"></td>
             </tr>
             <tr>
-                <td><label>Pesel: </label></td><td><input type="text" name="pesel"></td>
+                <td><label>Pesel:</label></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="pesel"></td>
             </tr>
            </table>
-           <label>Sortuj </label><select name="sort">
+           <label>Sortuj </label><select name="sort" id="sql">
                <option value="1">Domyślnie</option>
                <option value="2">Imiona alfabrtycznie</option>
                <option value="3">Nazwiska alfabrtycznie</option>
                <option value="4">Liczba punktów malejąco</option>
-               <option value="5">Liczba punktów rosnąco</option>
+               <option value="4">Liczba punktów rosnąco</option>
            </select><br>
            <input type="submit" value="Wyszukaj">
        </form>
     </div>
     <div id="main">
-    <table border="1">
+    <table border="1" class="ggez">
             
             <tr>
                 <td rowspan="2">ID</td><td rowspan="2">Imię</td><td rowspan="2">Nazwisko</td><td rowspan="2">PESEL</td><td colspan="4">Oceny</td><td colspan="3">Egzamin</td><td rowspan="2">Świadectwo z wyróżnieniem</td><td rowspan="2">Ilość punktów</td><td rowspan="2">Wybrana szkoła</td><td rowspan="2">Wybrany kierunek</td><td rowspan="2">Data dodania</td>
